d3c5b6c53c001d1862a2c227a7423e74ffaf6833
Vladislav <vlad.shaldymov@mail.ru>
2022-12-09 17:24:06 +0500

    init commit

---
+++ src/main/java/com/exchange/information/ExchangeInformationApplication.java
@@ -0,0 +1,13 @@
+package com.exchange.information;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeInformationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeInformationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exchange/information/ExchangeInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exchange.information;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeInformationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
