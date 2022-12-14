1d12bd30dd5cf4800a74f260dd498e990dcd9855
Vladislav <vlad.shaldymov@mail.ru>
2022-12-14 20:04:42 +0500

    EIS-6 add trade history CRUD endpoints

--- src/main/java/com/exchange/information/service/TradeHistoryService.java
+++ src/main/java/com/exchange/information/service/TradeHistoryService.java
@@ -2,6 +2,16 @@ package com.exchange.information.service;
 
 import com.exchange.information.model.TradeHistory;
 
+import java.util.List;
+
 public interface TradeHistoryService {
     TradeHistory save(TradeHistory tradeHistory);
+
+    List<TradeHistory> findAll();
+
+    List<TradeHistory> findAllBySecId(String SecId);
+
+    TradeHistory update(String secId, TradeHistory tradeHistory);
+
+    void delete(String id);
 }
--- src/main/java/com/exchange/information/service/impl/TradeHistoryServiceImpl.java
+++ src/main/java/com/exchange/information/service/impl/TradeHistoryServiceImpl.java
@@ -7,15 +7,50 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+import static java.lang.String.format;
+
 @Service
 @RequiredArgsConstructor
 public class TradeHistoryServiceImpl implements TradeHistoryService {
-    private final TradeHistoryRepository tradeHistoryRepository;
+    private final TradeHistoryRepository repository;
 
     @Transactional
     @Override
     public TradeHistory save(TradeHistory tradeHistory) {
-        return tradeHistoryRepository.save(tradeHistory);
+        return repository.save(tradeHistory);
     }
 
+    @Transactional(readOnly = true)
+    @Override
+    public List<TradeHistory> findAll() {
+        return repository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<TradeHistory> findAllBySecId(String id) {
+        return repository.findAllByHistoryId_Secid(id);
+    }
+
+    @Transactional
+    @Override
+    public TradeHistory update(String id, TradeHistory history) {
+        TradeHistory tradeHistory = repository.findByHistoryId_SecidAndHistoryId_TradeDate(id, history.getHistoryId().getTradeDate())
+                .orElseThrow(() -> new EntityNotFoundException(format("TradeHistory was not found with id=%s", id)));
+
+        tradeHistory.setNumtrades(history.getNumtrades());
+        tradeHistory.setOpen(history.getOpen());
+        tradeHistory.setClose(history.getClose());
+
+        return repository.save(history);
+    }
+
+    @Transactional
+    @Override
+    public void delete(String id) {
+        repository.deleteAll(repository.findAllByHistoryId_Secid(id));
+    }
 }
